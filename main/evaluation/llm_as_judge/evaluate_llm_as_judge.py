fb41463c7eaece388897200f2d56ccacd64ac81f
Nasiba Tuychieva <nasi_26@yahoo.de>
2026-01-08 04:56:54 +0100

    added new changes

--- main/evaluation/llm_as_judge/evaluate_llm_as_judge.py
+++ main/evaluation/llm_as_judge/evaluate_llm_as_judge.py
@@ -1071,7 +1071,7 @@ def aggregate_summary(results: List[EvaluationResult]) -> Dict[str, Any]:
 # -----------------------------
 def main() -> None:
     PROJECT_ROOT = Path(os.getenv("PROJECT_ROOT")).expanduser().resolve()
-    ANSWERS_PATH = PROJECT_ROOT / "main" / "evaluation" / "results" / "new" /"to_do_answers_log_new_llmgraph_reranker.jsonl"
+    ANSWERS_PATH = PROJECT_ROOT / "main" / "evaluation" / "results" / "new" /"to_do_answers_log_new_factual.jsonl"
     JSONL_IN = Path(os.getenv("ANSWERS_LOG_PATH", str(ANSWERS_PATH))).expanduser().resolve()
 
     log("Reading input JSONL", path=str(JSONL_IN))
